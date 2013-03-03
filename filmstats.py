d71a0f8ef700d4fa83643454125045d9ac724b10
Martin Sonntag <mail@martin-sonntag.de>
2013-03-03 15:58:56 +0100

    Working, except votes

--- filmstats.py
+++ filmstats.py
@@ -2,34 +2,54 @@ import imdb
 import oscarstats
 from pylab import *
 import re
+import sys
+import numpy
+
 ####this is the working branch for developing
 z= imdb.IMDb(accesSystem='http')
 
-b = zeros((250, 3))
-movie_list = z.get_top250_movies()
-for i in range(len(movie_list)):
-	print 'Nummer in der BestenListe: ' + str(i+1)
-	b[i-1, 0] = i
-	current_movie_name = movie_list[i]
-	print 'Filmname: ' +  str(current_movie_name)
-	current_movie_id = movie_list[i].movieID
-	current_movie = z.get_movie(current_movie_id)
-	laufzeit = current_movie['runtimes']
-	laufzeit = laufzeit[0]
-	laufzeit = re.findall(r'\d+', laufzeit)
-	laufzeit = laufzeit[0]
-	b[i-1, 1] = laufzeit
-	print 'Die Laufzeit betraegt: ' + laufzeit + ' Minuten'
-	current_no_of_oscars = oscarstats.get_no_oscars(current_movie_id)	
-	print 'Anzahl der Oscars: ' +  str(current_no_of_oscars)
-	b[i-1, 2] = current_no_of_oscars
-		
-	####Plotting####
+run_controller =  sys.argv[1]
+if run_controller == 'get_data':
+	b = zeros((250, 3))
+	movie_list = z.get_top250_movies()
+	for i in range(len(movie_list)):
+		print 'Nummer in der BestenListe: ' + str(i+1)
+		b[i-1, 0] = i
+		current_movie_name = movie_list[i]
+		print 'Filmname: ' +  str(current_movie_name)
+		current_movie_id = movie_list[i].movieID
+		current_movie = z.get_movie(current_movie_id)
+		laufzeit = current_movie['runtimes']
+		laufzeit = laufzeit[0]
+		laufzeit = re.findall(r'\d+', laufzeit)
+		laufzeit = laufzeit[0]
+		b[i-1, 1] = laufzeit
+		print 'Die Laufzeit betraegt: ' + laufzeit + ' Minuten'
+		current_no_of_oscars = oscarstats.get_no_oscars(current_movie_id)	
+		print 'Anzahl der Oscars: ' +  str(current_no_of_oscars)
+		b[i-1, 2] = current_no_of_oscars
+		savetxt("results.csv", b, delimiter=",")
+		####Plotting####
 
+
+		figure()
+    		plot(b[:,1], b[:,2], 'o' )
+    		xlabel('Movie runtime [min]')
+    		ylabel('Number of Oscars')
+    		grid(True)
+    		legend(['Number of Oscars'], 'upper right')
+		savefig('results_overview_runtime_preview.png', dpi=(800/8))
+
+if run_controller == 'post':
+	resultsMatrix = loadtxt('results.csv', delimiter=',',  skiprows=0)
 	figure()
-    	plot(b[:,1], b[:,2], 'o' )
+    	plot(resultsMatrix[:,1], resultsMatrix[:,2], 'o' )
     	xlabel('Movie runtime [min]')
     	ylabel('Number of Oscars')
     	grid(True)
     	legend(['Number of Oscars'], 'upper right')
 	savefig('results_overview.png', dpi=(800/8))
+	show()
+
+else:
+	print 'run option are get_data, postprocess, ...'
--- oscarstats.py
+++ oscarstats.py
@@ -5,9 +5,12 @@ z = imdb.IMDb(accesSystem='http')
 def get_no_oscars(id):
 	t = z.get_movie_awards(id)
 	t = t['data']
-	t = t['awards']
-	d=0
-	for s in range(len(t)):
-		if t[s]['award'] == 'Oscar' and t[s]['result'] == 'Won':
-			d=d+1
+	if len(t.keys()) == 0:
+		d = 0
+	else:
+		t = t['awards']
+		d=0
+		for s in range(len(t)):
+			if t[s]['award'] == 'Oscar' and t[s]['result'] == 'Won' and t[s]['assigner'] == 'Academy Awards, USA' :
+				d=d+1
 	return d
