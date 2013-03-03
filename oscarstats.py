d181a3f2dfa8749cd943d543d8274b94b3e0f5ed
Martin Sonntag <mail@martin-sonntag.de>
2013-03-03 20:31:17 +0100

    clean up of format problems

--- oscarstats.py
+++ oscarstats.py
@@ -3,20 +3,20 @@ import imdb
 z = imdb.IMDb(accesSystem='http')
 
 def get_no_oscars(id):
-	t = z.get_movie_awards(id)
-	t = t['data']
-	if len(t.keys()) == 0:
-		d = 0
-	else:
-		t = t['awards']
-		d=0
-		for s in range(len(t)):
-			if t[s]['award'] == 'Oscar' and t[s]['result'] == 'Won' and t[s]['assigner'] == 'Academy Awards, USA' :
-				d=d+1
-	return d
+    t = z.get_movie_awards(id)
+    t = t['data']
+    if len(t.keys()) == 0:
+        d = 0
+    else:
+        t = t['awards']
+        d=0
+        for s in range(len(t)):
+            if t[s]['award'] == 'Oscar' and t[s]['result'] == 'Won' and t[s]['assigner'] == 'Academy Awards, USA' :
+                d=d+1
+    return d
 
 
 def get_rating(id):
-	t = z.get_movie(id)
-	rating = t['rating']
-	return rating
+    t = z.get_movie(id)
+    rating = t['rating']
+    return rating
